87ffe47d99d51abcdc2d534e0fd122a92f779e07
Anders Einar Hilden <hildenae@gmail.com>
2022-12-17 21:02:07 +0100

    shellcheck

--- make-print.sh
+++ make-print.sh
@@ -7,15 +7,15 @@ fi
 START_NUM="$1"
 END_NUM="$(echo "$START_NUM + 15" | bc)"
 FILES=()
-for NUM in `seq -f "%07.0f" $START_NUM $END_NUM`; do
-  if [ -z $FIRST ]; then FIRST=$NUM fi;
+for NUM in $(seq -f "%07.0f" "$START_NUM" "$END_NUM"); do
+  if [ -z "$FIRST" ]; then FIRST="$NUM"; fi;
   LAST=$NUM;
   FILES+=("out-${NUM}.png")
-  qrencode --output="out-${NUM}.png" --type=PNG --ignorecase -s 30 -m 0 "H#$NUM"; 
+  qrencode --output="out-${NUM}.png" --type=PNG --ignorecase -s 30 -m 0 "H#$NUM";
   convert "out-${NUM}.png" -pointsize 130 -font "Ubuntu-Mono" -gravity center label:"H#$NUM" -trim -smush 20 "out-${NUM}.png"
 done;
-montage $FILES -density 600 -tile 4x4 -geometry +30+30 $FIRST-$LAST-out.png;
-ristretto $FIRST-$LAST-out.png
+montage "${FILES[@]}" -density 600 -tile 4x4 -geometry +30+30 "$FIRST-$LAST-out.png";
+ristretto "$FIRST-$LAST-out.png";
 #brother_ql --model QL-810W --printer tcp://brother-ql-810w.intern.hild1.no print -l 62red --red out.png.png
-mkdir "$FIRST-$LAST"
-mv *.png "$FIRST-$LAST/"
+mkdir "$FIRST-$LAST";
+mv ./*.png "$FIRST-$LAST/";
