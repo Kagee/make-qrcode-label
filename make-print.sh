5e7783fdd221fefc17e1fc9362fc09b2f3263ed9
Anders Einar Hilden <hildenae@gmail.com>
2022-12-17 21:38:28 +0100

    cleanup and comments

--- make-print.sh
+++ make-print.sh
@@ -15,6 +15,7 @@ fi
 START_NUM="$1"
 END_NUM="$(echo "$START_NUM + 15" | bc)"
 FILES=()
+# generate 16 zerofilled numbers from $START_NUM
 for NUM in $(seq -f "%07.0f" "$START_NUM" "$END_NUM"); do
   if [ -z "$FIRST" ]; then FIRST="$NUM"; fi;
   LAST=$NUM;
@@ -23,12 +24,16 @@ for NUM in $(seq -f "%07.0f" "$START_NUM" "$END_NUM"); do
   convert "out-${NUM}.png" -pointsize 130 -font "Ubuntu-Mono" -gravity center label:"H#$NUM" -trim -smush 20 "out-${NUM}.png"
 done;
 echo "Made image from H#$FIRST to H#$LAST"
+# Make a 4x4 (16) image montage
 montage "${FILES[@]}" -density 600 -tile 4x4 -geometry +30+30 "$FIRST-$LAST-out.png";
 ristretto "$FIRST-$LAST-out.png" &
-echo "risetto in background?"
 VIEWER_PID="$!"
+# Must sleep because it takes a second before we can find the window,
+# could in teory loop instead
 sleep 1
+# ristretto's title/name includes the filename
 VIEWER=$(xdotool search --onlyvisible --name "$FIRST-$LAST-out.png");
+# we must unmaximize (not minimize) before we move and resize
 wmctrl -ir "$VIEWER" -b remove,maximized_vert,maximized_horz
 xdotool windowmove "$VIEWER" 1920 550
 xdotool windowsize "$VIEWER" 500 500
