df766943a189c9592bae9b502ceeeb24f7a70123
Anders Einar Hilden <hildenae@gmail.com>
2023-03-19 18:17:14 +0100

    Refactor, merge one image twice, or two different. Print save or cancel.

--- print-dual-small.sh
+++ print-dual-small.sh
@@ -1,37 +1,66 @@
 #! /bin/bash
 
-source "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.env" 2>/dev/null || echo "Failed to load .env"; exit 1;
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "$SCRIPT_DIR/logging.sh" || { echo "Failed to load logging.sh"; exit 1; };
+source "$SCRIPT_DIR/.env" || { logThis "Failed to load .env." "INFO"; };
 
-echo "not using env vars"
-exit 1
-if [ -z "$1" ]; then
-  echo "Usage: <number>"
-exit 1
-fi
-
-
-set -o errexit
 set -o nounset
 
-STORAGE_DIR="./01_Output"
-INPUT="$(printf "%07.0f" "$1").png"
+if [ -z "${1-}" ]; then
+  echo "Usage: $0 NUMBER [NUMBER]"
+  exit 1
+fi
 
-trap ctrl_c INT
+CHR1="${1:0:1}"
+CHR1="${CHR1^}"
+NUM1="$(printf "%07.0f" "${1:1}")"
 
-function ctrl_c() {
-        echo "Cleaning up"
-        rm -- *.png 2>/dev/null
-}
+CHR2="$CHR1"
+NUM2="$NUM1"
 
-if [ ! -f "$STORAGE_DIR/$INPUT" ]; then
-  echo "Input file $STORAGE_DIR/$INPUT does not exist." 1>&2
+if [ -n "${2-}" ]; then
+  CHR2="${2:0:1}"
+  CHR2="${CHR2^^}"
+  NUM2="$(printf "%07.0f" "${2:1}")"
+fi
+
+PAT1="$QK_ARCHIVE_BASE/$CHR1/$NUM1/label/"
+PAT2="$QK_ARCHIVE_BASE/$CHR1/$NUM1/label/"
+IMG1="$PAT1/${NUM1}.png"
+IMG2="$PAT2/${NUM2}.png"
+
+if [ ! -f "$IMG1" ]; then
+  logThis "Input file $IMG1 ($1) does not exist." "ERROR"
+  exit 1
+fi
+
+if [ ! -f "$IMG2" ]; then
+  logThis "Input file $IMG2 ($2) does not exist." "ERROR"
   exit 1
 fi
 
 TMPFILE="$(mktemp "/tmp/$0.XXXXXX.png")"
 
-convert "$STORAGE_DIR/$INPUT" "$STORAGE_DIR/$INPUT" +append "$TMPFILE"
+convert "$IMG1" "$IMG2" +append "$TMPFILE"
+
+$QK_TIV_EXEC "$TMPFILE"
+
+# Pipe input from /dev/tty so this can be used in a while loop (subshell)
+echo "[P/s/c] Print and save, just Save or Cancel"
+read -r -n 1 COMMAND < /dev/tty;
+echo
+if [ -n "$COMMAND" ]; then
+  if [[ "$COMMAND" =~ ^(S|s)$ ]]; then
+    cp "$TMPFILE" "$PAT1/${CHR1}-${NUM1}-${CHR2}-${NUM2}.png"
+    mv "$TMPFILE" "$PAT2/${CHR1}-${NUM1}-${CHR2}-${NUM2}.png"
+    echo "Saved to $PAT1/${CHR1}-${NUM1}-${CHR2}-${NUM2}.png and $PAT2/${CHR1}-${NUM1}-${CHR2}-${NUM2}.png"
+    exit 0;
+  elif [[ "$COMMAND" =~ ^(C|c)$ ]]; then
+    rm "$TMPFILE"
+    exit 0
+  fi
+fi
 
 brother_ql --model QL-810W --printer tcp://brother-ql-810w.intern.hild1.no print -l 62 "$TMPFILE"
-
-rm "$TMPFILE"
+cp "$TMPFILE" "$PAT1/${CHR1}-${NUM1}-${CHR2}-${NUM2}.png"
+mv "$TMPFILE" "$PAT2/${CHR1}-${NUM1}-${CHR2}-${NUM2}.png"
